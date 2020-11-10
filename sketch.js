42d9b66b0bbc5fbe1043e0da5b43d29d9b72fe1d
Nisarg Thakar <62792393+nisargCR7@users.noreply.github.com>
2020-11-10 09:52:50 +0530

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -655,3 +655,43 @@ if(mstate==="down"){
 
 
 }
+function touchMoved(){
+
+if(mstate==="right"){
+  mazerunner.velocityX=0
+  mazerunner.velocityY=0
+  mazerunner.changeAnimation("right",right_stand)
+  mstate=0
+  mstate2=0 
+}
+
+
+if(mstate==="left"){
+  mazerunner.velocityX=0
+  mazerunner.velocityY=0
+  mazerunner.changeAnimation("left",left_stand)
+  mstate=0
+  mstate2=0 
+}
+
+
+if(mstate==="up"){
+ mazerunner.velocityX=0
+ mazerunner.velocityY=0
+ mazerunner.changeAnimation("up",up_stand)
+ mstate=0
+ mstate2=0 
+}
+
+
+if(mstate==="down"){
+ mazerunner.velocityX=0
+ mazerunner.velocityY=0
+ mazerunner.changeAnimation("down",down_stand)
+ mstate=0
+ mstate2=0 
+}    
+
+
+
+}
